306b28ff95978fe944d9a6d8bd9f9512a5644fa4
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-03-19 23:54:00 +0530

    Building First Version of Module

--- index.js
+++ index.js
@@ -5,4 +5,41 @@
  *
  *
  */
-module.exports = () => {};
+const chalk = require("chalk");
+const sym = require("log-symbols");
+const log = console.log;
+const green = chalk.green;
+const greenI = chalk.green.bold.inverse;
+const orange = chalk.keyword("orange");
+const orangeI = chalk.keyword("orange").bold.inverse;
+const red = chalk.red;
+const redI = chalk.red.bold.inverse;
+const blue = chalk.blue;
+const blueI = chalk.blue.bold.inverse;
+
+module.exports = options => {
+  const defaultOptions = {
+    type: `error`,
+    msg: `You forgot to define Options`,
+    name: null
+  };
+  const opt = { ...defaultOptions, ...options };
+  const { type, msg, name } = opt;
+  const printName = name ? name.toUpperCase() : type.toUpperCase();
+
+  if (type === "success") {
+    log(`\n${sym.success} ${greenI(` ${printName} `)} ${green(msg)}\n`);
+  }
+
+  if (type === "warning") {
+    log(`\n${sym.warning} ${orangeI(` ${printName} `)} ${orange(msg)}\n`);
+  }
+
+  if (type === "error") {
+    log(`\n${sym.error} ${redI(` ${printName} `)} ${red(msg)}\n`);
+  }
+
+  if (type === "info") {
+    log(`\n${sym.info} ${blueI(` ${printName} `)} ${blue(msg)}\n`);
+  }
+};
--- test.js
+++ test.js
@@ -1,3 +1,47 @@
 const cliAlerts = require("./index.js");
 
-cliAlerts();
+// With Default Name
+cliAlerts({
+  type: "error",
+  msg: "Error Msg"
+});
+
+cliAlerts({
+  type: "success",
+  msg: "SUccess Msg"
+});
+
+cliAlerts({
+  type: "warning",
+  msg: "Warning Msg"
+});
+
+cliAlerts({
+  type: "info",
+  msg: "INfo Msg"
+});
+
+// With Name
+cliAlerts({
+  type: "error",
+  msg: "Error Msg",
+  name: "Errrora"
+});
+
+cliAlerts({
+  type: "success",
+  msg: "SUccess Msg",
+  name: "Problem"
+});
+
+cliAlerts({
+  type: "warning",
+  msg: "Warning Msg",
+  name: "Careful"
+});
+
+cliAlerts({
+  type: "info",
+  msg: "INfo Msg",
+  name: "Nothing"
+});
